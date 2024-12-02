b52d7ef7ddb4a085d2175329b5f432b61277d4d3
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-12-02 19:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
+
+ 
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
